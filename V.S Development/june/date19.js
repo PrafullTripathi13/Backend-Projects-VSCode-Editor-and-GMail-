6c1bead96b7737370864ba593037b8522d47c9af
Prafull Tripathi <114906363+PrafullTripathi13@users.noreply.github.com>
2024-09-15 18:15:18 +0530

    Add files via upload

---
+++ V.S Development/june/date19.js
@@ -0,0 +1,32 @@
+// 1. Ek array hai jisme 5 members h, aap us array k members ka sum print kro
+
+//var arr = [10,20,30,'40',50];
+//var sum = 0;
+//arr.forEach(function(elem){
+    //if(typeof elem === "number"){
+     //   sum = sum+elem;
+    //}
+//})
+//console.log(sum);
+
+// 2. Ask a string and only print it if it has more than four(4) character
+
+//var str = prompt("enter string");
+//if(str.length>4){
+  //  console.log(str);
+//}
+
+// 3. Ask a string and make another string with its alternate characters and print it.
+
+//var s=prompt("enter string");
+//var extra="";
+//for(var i=0; i<s.length; i++)
+//{
+ //   if(i%2===0){
+  //      extra==extra+s[i];
+    //}
+    //else{
+      //  extra=extra+" ";
+   // }
+//}
+//console.log(extra);
